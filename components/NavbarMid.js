f2348ff303816b6f65c07c4aa14c72442e4438e9
samrmdhn <thesyams.rebel8@gmail.com>
2022-06-22 23:48:33 +0700

    first commit

---
+++ components/NavbarMid.js
@@ -0,0 +1,15 @@
+import React from "react";const NavbarMid = () => {
+  return (
+    <>
+      <div style={{ fontWeight: "bolder", borderBottom: "2px solid black" }}>
+        <div style={{ marginTop: 16 }} className="row">
+          <div className="col-lg-4">Dashboard</div>
+          <div className="col-lg-4"></div>
+          <div className="col-lg-4">Bandung</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarMid;
